128f0ca230cda14efa2fdd4a500a5bda098c4409
enot143 <arina.stenina@mail.ru>
2022-11-25 14:07:36 +0300

    -

--- service2/src/main/java/itmo/service2/services/RouteService.java
+++ service2/src/main/java/itmo/service2/services/RouteService.java
@@ -1,5 +1,7 @@
 package itmo.service2.services;
 
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
 import itmo.service2.dto.LocationDto;
 import itmo.service2.dto.RouteDto;
 import itmo.service2.dto.RoutePostDto;
@@ -12,11 +14,13 @@ import java.util.List;
 
 @Service
 public class RouteService {
-    public static final String BASE_URL = "http://localhost:4567/soa-3-1.0-SNAPSHOT";
+
+    public final String BASE_URL = getBaseUrl();
 
     RestTemplate restTemplate = new RestTemplate();
 
     public ResponseEntity<List<Route>> findRouteByIds(Integer idFrom, Integer idTo, String order) {
+
         String URI = BASE_URL + "/routes?from_id=between:"
                 + idFrom + ":" + idFrom + "&to_id=between:" + idTo + ":" + idTo +
                 "&sort=" + order;
@@ -35,6 +39,13 @@ public class RouteService {
         System.out.println(routePost);
         String URI = BASE_URL + "/routes";
         return restTemplate.postForEntity(URI, routePost, ResponseEntity.class);
+    }
+
+    private String getBaseUrl(){
+        Consul client = Consul.builder().build();
+        HealthClient agentClient = client.healthClient();
+        com.orbitz.consul.model.health.Service s = agentClient.getHealthyServiceInstances("soa-3-1.0-SNAPSHOT").getResponse().get(0).getService();
+        return "http://" + s.getAddress() + ":" + s.getPort() + "/" + s.getId();
     }
 }
 
